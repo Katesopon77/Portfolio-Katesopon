8802944a553a0c2b17a466c56312b71f9170245d
Katesopon <36659000+Katesopon77@users.noreply.github.com>
2020-09-09 12:57:21 +0700

    Alstar-Template

---
+++ Alstar-starter/js/javascript.js
@@ -0,0 +1,8 @@
+function myFunction(){
+    var navbar = document.getElementById('menubar');
+    if (navbar.style.display ==='block') {
+        navbar.style.display ='none';
+    } else {
+        navbar.style.display ='block';
+    }
+}
